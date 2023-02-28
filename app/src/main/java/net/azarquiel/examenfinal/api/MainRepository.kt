6edf91a562eeacc347d0adf0bc83a14b24d0d40f
LunaryLayn <hupogo2000@protonmail.com>
2023-02-28 19:22:01 +0100

    V0

---
+++ app/src/main/java/net/azarquiel/examenfinal/api/MainRepository.kt
@@ -0,0 +1,4 @@
+package net.azarquiel.examenfinal.api
+
+class MainRepository {
+}
---
+++ app/src/main/java/net/azarquiel/examenfinal/api/WebAccess.kt
@@ -0,0 +1,14 @@
+package net.azarquiel.examenfinal.api
+
+object WebAccess {
+   /* val avesService : AvesService by lazy {
+
+        val retrofit = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .baseUrl("http://www.ies-azarquiel.es/paco/apiaves/")
+            .build()
+
+        return@lazy retrofit.create(AvesService::class.java)
+    }*/
+}
