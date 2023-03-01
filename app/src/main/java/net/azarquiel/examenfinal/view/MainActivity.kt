777892eaacafbfe78dd5541b84052dd514ab4657
LunaryLayn <hupogo2000@protonmail.com>
2023-03-01 09:14:25 +0100

    V1.1 - Pantalla inicial con seach

--- app/src/main/java/net/azarquiel/examenfinal/view/MainActivity.kt
+++ app/src/main/java/net/azarquiel/examenfinal/view/MainActivity.kt
@@ -5,6 +5,7 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -14,7 +15,8 @@ import net.azarquiel.examenfinal.databinding.ActivityMainBinding
 import net.azarquiel.examenfinal.entities.Categoria
 import net.azarquiel.examenfinal.viewmodel.MainViewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
+    private lateinit var searchView: SearchView
 
     private lateinit var categorias: List<Categoria>
     private lateinit var adapter: CategoriaAdapter
@@ -53,16 +55,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        searchView = searchItem.actionView as SearchView
+        searchView.setQueryHint("Search...")
+        searchView.setOnQueryTextListener(this)
+
         return true
     }
+
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_login -> {
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+    //Buscador
+    override fun onQueryTextChange(query: String): Boolean {
+        val original = ArrayList<Categoria>(categorias)
+        adapter.setCategorias(original.filter { cat -> cat.nombre.contains(query,true) })
+        return false
+    }
+
+    override fun onQueryTextSubmit(text: String): Boolean {
+        return false
+    }
+
+
 }
